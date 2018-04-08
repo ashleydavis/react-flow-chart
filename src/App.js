016f8582d17ecb5adc270f9533370f62831368e7
Ashley Davis <ashley@codecapers.com.au>
2018-04-08 20:04:52 +1000

    Added node text as well. Added a group to contain the rect and text and transform it based on x/y props. Also rendering the name for the node.

--- src/App.js
+++ src/App.js
@@ -9,25 +9,43 @@ class Node extends Component {
   }
 
   render() {
+    const x = this.props.x;
+    const y = this.props.y;
+    //TODO: Why can't I just include this directly in the DOM below???
+    const translate = `translate(${x}, ${y})`;
     return (
-      <rect
-        className="node-rect"
-        ry="10"
-        rx="10"
-        x="0"
-        y="0"
-        width={this.props.width}
-        height={this.props.height}
-        fill="gray"
-      >
-      </rect>            
+      <g
+        transform={translate}
+        >
+        <rect
+          className="node-rect"
+          ry="10"
+          rx="10"
+          x="0"
+          y="0"
+          width={this.props.width}
+          height={this.props.height}
+          fill="gray"
+        />
+        <text
+            x={this.props.width/2}
+            y="25"
+            textAnchor="middle"
+            alignmentBaseline="middle"
+            >      
+            {this.props.name}          
+          </text>
+      </g>
     );
   }
 }
 
 Node.propTypes = {
+  x: PropTypes.number.isRequired,
+  y: PropTypes.number.isRequired,
   width: PropTypes.number.isRequired,
   height: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
 };
 
 class App extends Component {
@@ -39,10 +57,16 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <div>
-          <svg>
+          <svg
+            width={600}
+            height={600}
+            >
             <Node 
+              x={120}
+              y={75}
               width={200}
               height={50}
+              name="Test node"
               />
           </svg>
         </div>
