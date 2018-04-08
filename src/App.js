92f970d617d69e6325ce4c5e2328d3dab60441dd
Ashley Davis <ashley@codecapers.com.au>
2018-04-08 19:45:52 +1000

    Fixed the Node component and added required number properties for width and height.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import PropTypes from 'prop-types';
 import './App.css';
 
 class Node extends Component {
+  constructor(props) {
+    super(props)   
+  }
+
   render() {
     return (
       <rect
-        class="node-rect"
+        className="node-rect"
         ry="10"
         rx="10"
         x="0"
         y="0"
-        width="150"
-        height="50"
+        width={this.props.width}
+        height={this.props.height}
         fill="gray"
       >
       </rect>            
     );
   }
 }
+
+Node.propTypes = {
+  width: PropTypes.number.isRequired,
+  height: PropTypes.number.isRequired,
+};
 
 class App extends Component {
   render() {
@@ -30,7 +40,10 @@ class App extends Component {
         </header>
         <div>
           <svg>
-            <Node />
+            <Node 
+              width={200}
+              height={50}
+              />
           </svg>
         </div>
       </div>
